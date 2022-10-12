b2742e3956a955b854e69006cb29229d8e306164
Pedro Castro <aspeddro@gmail.com>
2022-10-12 12:22:58 -0300

    nvim: update globals.lua

--- .config/nvim/lua/user/globals.lua
+++ .config/nvim/lua/user/globals.lua
@@ -3,10 +3,27 @@ RELOAD = function(...)
 end
 
 P = function(v)
-  print(vim.inspect(v))
-  return v
+  vim.pretty_print(v)
 end
 
 R = function(name)
   return RELOAD(name)
+end
+
+---Print table to buffer
+PB = function(v)
+  if type(v) ~= 'table' then
+    return P(v)
+  else
+    local bufnr = vim.api.nvim_create_buf(false, false)
+    vim.api.nvim_buf_set_lines(
+      bufnr,
+      0,
+      -1,
+      true,
+      vim.split(vim.inspect(v), '\n', { plain = true })
+    )
+    vim.cmd.vsplit()
+    vim.api.nvim_win_set_buf(vim.api.nvim_get_current_win(), bufnr)
+  end
 end
